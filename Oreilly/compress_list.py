198c7f0d3fd7dde29eedc0817314b2bb805fd7ef
Anton Dovnar <fode4cun@gmail.com>
2021-03-05 11:42:46 +0300

    Checkio

--- Oreilly/compress_list.py
+++ Oreilly/compress_list.py
@@ -12,16 +12,22 @@
 # END_DESC
 
 from typing import Iterable
+from itertools import groupby
+
+
+# def compress(items: list) -> Iterable:
+#     new_list = []
+
+#     for i in range(len(items)):
+#         if i == 0 or items[i] != items[i-1]:
+#             new_list.append(items[i])
+
+#     return new_list
 
 
 def compress(items: list) -> Iterable:
-    new_list = []
-
-    for i in range(len(items)):
-        if i == 0 or items[i] != items[i-1]:
-            new_list.append(items[i])
-
-    return new_list
+    for key, _ in groupby(items):
+        yield key
 
 
 if __name__ == '__main__':
